d36e72673b8cd08b0732498d0dcc8615901e884e
Takatoshi Ishiguro <ishiguro.taka@gmail.com>
2024-04-11 21:10:50 +0900

    コントローラの作成

--- database/migrations/2024_04_11_115751_create_articles_table.php
+++ database/migrations/2024_04_11_115751_create_articles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $title->text('body');
+            $table->text('body');
             $table->timestamps();
         });
     }
